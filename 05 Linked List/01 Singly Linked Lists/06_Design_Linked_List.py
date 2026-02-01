ce41e5504fe4c48ba95977c0a3c44e7ae5723ecd
NSR Dev <nsr.dev.in@gmail.com>
2026-02-01 11:05:54 +0530

    Add files via upload

---
+++ 05 Linked List/01 Singly Linked Lists/06_Design_Linked_List.py
@@ -0,0 +1,57 @@
+class listnode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = listnode(0)  # dummy node
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+
+        curr = self.head.next
+        for _ in range(index):
+            curr = curr.next
+        return curr.val
+-
+    def addAtHead(self, val: int) -> None:
+        node = listnode(val)
+        node.next = self.head.next
+        self.head.next = node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        node = listnode(val)
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+
+        node = listnode(val)
+        node.next = curr.next
+        curr.next = node
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+
+        curr.next = curr.next.next
+        self.size -= 1
